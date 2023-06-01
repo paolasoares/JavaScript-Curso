430d6d428ac6a7cf65f8daffcc1927753b75fa20
PaolaSoares <soaresgpaola@gmail.com>
2023-06-01 19:43:30 -0300

    Aulas

---
+++ number/math.js
@@ -0,0 +1,9 @@
+let num1 = 8.50
+// let num2 = Math.floor(num1);//deixar o numero inteiro arredondando pra baixo.
+// let num2 = Math.ceil(num1);//deixar o numero inteiro arredondando pra cima.
+//let num2 = Math.round(num1);
+// console.log(Math.max(1,2,3,4,5,-56,-90,-1000,2,4)); //maior number
+// console.log(Math.min(1,2,3,4,5,-56,-90,-1000,2,4));//menor number
+
+// const aleatorio = Math.round(Math.random()*(10 - 5 )+ 5); //numero aleatoria 
+// console.log(aleatorio); //number aleatorio
---
+++ number/number.js
@@ -0,0 +1,15 @@
+let num1 = 0.7
+let num2 = 0.1
+
+num1 = Number(num1.toFixed(2)) //calcular numeros de casas decimais
+console.log(num1)
+console.log(Number.isInteger(num1))
+
+
+// console.log(num1.toString()+ num2) //saber o tipo 
+// num1 = num1.toString();
+// console.log(num1.toString(2)) //numero não binario
+// console.log(num1.toFixed(2)) //arendondar o numero em casas dicimais
+// console.log(Number.isInteger(num1)) //vai retorna pra mim verdadeiro ou falso.
+// console.log(Number.isNaN(tem))
+// let tem  = num1* '3'
