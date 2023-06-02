1b32c3d2a973557946de42d9a63d6eabf99e56cb
PaolaSoares <soaresgpaola@gmail.com>
2023-06-02 00:10:51 -0300

    arrays

---
+++ Arrays/ArraysBasico/array.js
@@ -0,0 +1,40 @@
+const alunos = ['Paola', 'Anna','Lucia','Julia']
+
+
+console.log(alunos)
+
+//slice - fatia a lista
+
+//apaga um indice da list
+// delete alunos [0]; 
+
+// remove o primeiro item da lista
+// const removido = alunos.shift();
+
+//remove o ultimo item da lista
+// const removido = alunos.pop();
+// console.log(removido)
+
+
+// adiciona o nome no inicio do array
+// alunos.unshift('Naza')
+// alunos.unshift('Pedro')
+
+// Adiciona o final do array, melhor forma de adicionar
+// alunos.push('Pedro')
+// alunos.push('Naza')
+
+//adiciona outros nomes no final do array
+// alunos[alunos.length] = 'Naza'
+// alunos[alunos.length] = 'Julio'
+// alunos[alunos.length] =' Pedro'
+
+
+//trocar o nome 
+// alunos[0] = 'carla'
+
+//posição do nome no arrey
+// console.log(alunos)
+// console.log(alunos[0])
+// console.log(alunos[3])
+
