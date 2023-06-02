185a1cd990a0287f611cb953ec2ec33de20bb781
PaolaSoares <soaresgpaola@gmail.com>
2023-06-02 01:06:48 -0300

    .

---
+++ Funcoes/FuncoesBasico/funcoes.js
@@ -0,0 +1,31 @@
+//arrow function =>
+const raiz = (n) => n ** 0.5
+
+console.log(raiz(9))
+console.log(raiz(16))
+console.log(raiz(25))
+
+
+// const raiz = function(n){
+//     return n ** 0.5;
+// };
+// console.log(raiz(9))
+// console.log(raiz(16))
+// console.log(raiz(25))
+
+
+// função soma de 2 valores
+// function soma(x, y){
+//     const resultado = x + y;
+//     return resultado;
+// }
+// console.log(soma(2,2))
+
+
+
+// função que tem um retorno
+// function saudacao(nome){
+//     return`Boa Noite ${nome}!`;     
+// }
+// const variavel = saudacao('Paola');
+// console.log(variavel)
