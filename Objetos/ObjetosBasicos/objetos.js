829108b8451a14152181ec01d03f2b99bc20a0de
PaolaSoares <soaresgpaola@gmail.com>
2023-06-27 00:01:16 -0300

    objetos

--- Objetos/ObjetosBasicos/objetos.js
+++ Objetos/ObjetosBasicos/objetos.js
@@ -1,3 +1,37 @@
+const pessoa = {
+    nome: 'Paola',
+    sobrenome: 'Soares',
+    idade: 22,
+
+    fala(){
+        console.log(`${this.nome} ${this.sobrenome} está falando, Hi... `),
+        console.log(`${this.idade} está e minha idade`)
+    },
+    incrementaIdade(){
+        this.idade++;
+    }
+
+};
+pessoa.fala()
+pessoa.incrementaIdade()
+pessoa.fala()
+pessoa.incrementaIdade()
+pessoa.fala()
+pessoa.incrementaIdade()
+
+
+
+// function criaPessoa (nome, sobrenome, idade){
+//     return{
+//         nome,
+//         sobrenome,
+//         idade
+//     };
+// }
+// const pessoa1 = criaPessoa('Paola', 'Soares', 42)
+// const pessoa2 = criaPessoa('Anna', 'Alves', 27)
+// const pessoa3 = criaPessoa('Julia', 'souza', 12)
+// console.log(pessoa1.nome, pessoa2.nome)
 
 
 
