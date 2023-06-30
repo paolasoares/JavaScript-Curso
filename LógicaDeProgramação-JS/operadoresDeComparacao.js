e6c7f424cc07311ed4075353171bf2e4ff396832
PaolaSoares <soaresgpaola@gmail.com>
2023-06-30 02:03:36 -0300

    Organizando

---
+++ LógicaDeProgramação-JS/operadoresDeComparacao.js
@@ -0,0 +1,13 @@
+/*
+Operadores de comparação    ""
+> maior que
+>= maior que ou igual a
+< menor que 
+<= menor que ou igual a
+== igualdade (valor)
+=== igualdade estrita (valor e tipo)
+!= diferente
+!== diferente estrito
+//
+(= atribuição)
+*/
